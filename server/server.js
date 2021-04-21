45c2a674aff93616a8cb1f2b90e0e057a3a5d052
Sourabh <sourabh.khera@tothenew.com>
2021-04-21 11:50:14 +0530

    changes made in server.js

--- server/server.js
+++ server/server.js
@@ -1,23 +1,12 @@
 const express = require("express");
-const webpack = require("webpack");
-const bodyParser = require("body-parser");
-const webpackconfig = require("../../webpack.config");
-const webpackMiddleware = require("webpack-dev-middleware");
-const compiler = webpack(webpackconfig);
 const routes = require("./routes/routes");
 require("./configuraion/dataSource");
 const app = express();
-app.use(webpackMiddleware(compiler, {
-    hot: true,
-    publicPath: '/',
-    historyApiFallback: true,
-}));
-app.use(bodyParser());
+app.use(express.json());
 routes(app);
 
 const PORT = 3000;
 
-
-app.listen(PORT,()=>{
-console.log("server is listening on port number------>",PORT)
+app.listen(PORT, () => {
+  console.log("server is listening on port number------>", PORT);
 });
